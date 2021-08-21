7943328e490f80e24b10a8e82ae022ceac38da8c
hajipong <hajipong@gmail.com>
2021-08-22 03:18:45 +0900

    基本部分実装

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# family_tree
+# 家系図つくるやつ
+
+* person: 人物の四角
+  * row: 何段目
+  * column: 何列目
+    - 1段目は0,2,4　2段目は1,3,5と指定すると綺麗に入るようにした。
+* marriage: 結婚線、2人のpersonのcolumnを指定する。
+* child: 子供線、親の間のcolumn（親が2と4なら3を入れる）と子のcolumnを入れる。
+* child_single_person: 片親のみの子供線
---
+++ family_tree.js
@@ -0,0 +1,91 @@
+const person_width = 100;
+const person_height = 60;
+const person_x = 80;
+const person_y = 100;
+
+function person(row, column) {
+    rect(row, column);
+}
+
+function marriage(row, column_1, column_2) {
+    line(person_right_x(column_1),
+        marriage_y(row),
+        person_left_x(column_2),
+        marriage_y(row));
+}
+
+function child(row_1, column_1, row_2, column_2) {
+    child_line(row_1, column_1, row_2, column_2, marriage_y(row_1))
+}
+
+function child_single_parent(row_1, column_1, row_2, column_2) {
+    child_line(row_1, column_1, row_2, column_2, person_bottom_y(row_1))
+}
+
+function child_line(row_1, column_1, row_2, column_2, start_y) {
+    line(person_center_x(column_1),
+        start_y,
+        person_center_x(column_1),
+        child_branch_y(row_2));
+    line(person_center_x(column_1),
+        child_branch_y(row_2),
+        person_center_x(column_2),
+        child_branch_y(row_2));
+    line(person_center_x(column_2),
+        child_branch_y(row_2),
+        person_center_x(column_2),
+        person_top_y(row_2));
+}
+
+function person_right_x(column) {
+    return column * person_x + person_width;
+}
+
+function person_left_x(column) {
+    return column * person_x;
+}
+
+function person_top_y(row) {
+    return (row) * person_y;
+}
+
+function person_bottom_y(row) {
+    return (row) * person_y + person_height;
+}
+
+function person_center_x(column) {
+    return person_left_x(column) + person_width / 2;
+}
+
+function marriage_y(row) {
+    return row * person_y + (person_height / 2);
+}
+
+function child_branch_y(row) {
+    return person_top_y(row) - (person_y - person_height) / 2;
+}
+
+function rect(row, column) {
+    let rect = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
+    rect.setAttribute('x', column * person_x);
+    rect.setAttribute('y', row * person_y);
+    rect.setAttribute('width', person_width);
+    rect.setAttribute('height', person_height);
+    rect.setAttribute('rx', 10);
+    rect.setAttribute('ry', 10);
+    rect.setAttribute('fill', '#EEE');
+    document.querySelector('svg.family_tree').appendChild(rect);
+}
+
+function line(x1, y1, x2, y2) {
+    let line = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+    line.setAttribute('x1', x1);
+    line.setAttribute('y1', y1);
+    line.setAttribute('x2', x2);
+    line.setAttribute('y2', y2);
+    line.setAttribute('fill', 'none');
+    line.setAttribute('stroke', '#000');
+    line.setAttribute('stroke-linejoin', 'round');
+    line.setAttribute('stroke-width', '1');
+    document.querySelector('svg.family_tree').appendChild(line);
+}
